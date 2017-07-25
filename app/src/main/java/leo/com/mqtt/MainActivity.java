bc412c157fe1d91e0ff9956d62af40cd3179d63a
Leo <leolai728@live.com>
2017-07-25 09:35:12 +0800

    Add in convert hexa string to byte Add in comment Check the action

--- app/src/main/java/leo/com/mqtt/MainActivity.java
+++ app/src/main/java/leo/com/mqtt/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radMale,radFemale;
     Spinner spinner;
     TextView txtViewEncodeResult;
-    static int decodeCurrentCursor = 0;
+    static int decodeCurrentCursor = 0; // To check current cursor, every cursor checked, will increase the number check
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,39 +34,104 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void btnDecode(View v) {//525344
-        String subscribeData = "124c6565205761682050656e67000002525344000003066c656f313233";
-        if(subscribeData.length()%2 == 0){
-            char nameLengthFirstDigit = subscribeData.charAt(0);
-            char nameLengthSecondDigit = subscribeData.charAt(1);
-            decodeCurrentCursor += 1;
-            int nameSize = Integer.parseInt(String.valueOf(nameLengthFirstDigit)+String.valueOf(nameLengthSecondDigit));
-            editTextName.setText(hexToAscii(splitString(nameSize,subscribeData)));
+    public void btnDecode(View v) {
+        decodeCurrentCursor = 0;
 
-            if(subscribeData.charAt(decodeCurrentCursor + 6) == '1'){
-                radMale.setChecked(true);
-            }else if(subscribeData.charAt(decodeCurrentCursor + 6) == '2'){
-                radFemale.setChecked(true);
+        //                 Prepare message publish to MQTT Server
+        //===============================================================================
+        // 1 = Login Action
+        // 0 = Researve
+        // 124c6565205761682050656e67 = 12 Length of Username (First 2 ASCll character)
+        //                            = 4c6565205761682050656e67 Username
+        // 066c656f313233 = 06 Length of password (First 2 ASCll character)
+        //                = 6c656f313233 Password
+        String sendSubscibeData = "10124c6565205761682050656e67066c656f313233";
+
+        // Alert show what data publish to server
+        AlertDialog.Builder publishBuilder = new AlertDialog.Builder(this);
+        publishBuilder.setMessage("Data publish: " + sendSubscibeData)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        AlertDialog publishAlert = publishBuilder.create();
+        publishAlert.show();
+
+        //           Perform publish and receive publish from MQTT Server
+        //================================================================================
+
+        // Here to perform publish data to MQTT server and receive data publish
+        // from MQTT server (Code by yourself)
+
+        //=================================================================================
+
+
+        //             Receive data publish from MQTT Server and split it
+        //=================================================================================
+        // 5 = Respond from server
+        // 0 = Reserve
+        // 124c6565205761682050656e67 = 12 Length of Username (First 2 ASCll character)
+        //                            = 4c6565205761682050656e67 Username
+        //
+        // 000002 = Female (Gender)
+        //
+        // 525344 = RSD (Programme Code)
+        //
+        // 000003 = 3 (Year of study)
+        //
+        // 066c656f313233 = 06 Length of password (First 2 ASCll character)
+        //                = 6c656f313233 Password
+        String receiveSubscribeData = "50124c6565205761682050656e67000002525344000003066c656f313233"; // Sample receive data from MQTT Server
+        if(receiveSubscribeData.length()%2 == 0){
+            if(receiveSubscribeData.charAt(decodeCurrentCursor) == '5'){ // Check if the server return command 5 = Login Respond
+
+                decodeCurrentCursor += 1;
+
+                char nameLengthFirstDigit = receiveSubscribeData.charAt(decodeCurrentCursor + 1);
+                char nameLengthSecondDigit = receiveSubscribeData.charAt(decodeCurrentCursor + 2);
+                decodeCurrentCursor += 2;
+                int nameSize = Integer.parseInt(String.valueOf(nameLengthFirstDigit)+String.valueOf(nameLengthSecondDigit));
+                editTextName.setText(hexToAscii(splitString(nameSize,receiveSubscribeData)));
+
+                if(receiveSubscribeData.charAt(decodeCurrentCursor + 6) == '1'){
+                    radMale.setChecked(true);
+                }else if(receiveSubscribeData.charAt(decodeCurrentCursor + 6) == '2'){
+                    radFemale.setChecked(true);
+                }else{
+
+                }
+                decodeCurrentCursor += 6;
+
+                String programmeCode = hexToAscii(splitString(3,receiveSubscribeData)); //Decode from hex to ASCll String
+                if(programmeCode.equals("RSD")){
+                    spinner.setSelection(0);
+                }else if(programmeCode.equals("RIT")){
+                    spinner.setSelection(1);
+                }
+
+                editTextYear.setText(receiveSubscribeData.charAt(decodeCurrentCursor + 6)+"");
+                decodeCurrentCursor += 6;
+
+                char passwordLengthFirstDigit = receiveSubscribeData.charAt(decodeCurrentCursor+1);
+                char passwordLengthSecondDigit = receiveSubscribeData.charAt(decodeCurrentCursor+2);
+                decodeCurrentCursor += 2;
+                int passwordSize = Integer.parseInt(String.valueOf(passwordLengthFirstDigit)+String.valueOf(passwordLengthSecondDigit));
+                editTextPassword.setText(hexToAscii(splitString(passwordSize,receiveSubscribeData)));
             }else{
 
+                publishBuilder.setMessage("Receive publish")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                publishAlert.show();
             }
-            decodeCurrentCursor += 6;
 
-            String programmeCode = hexToAscii(splitString(3,subscribeData));
-            if(programmeCode.equals("RSD")){
-                spinner.setSelection(0);
-            }else if(programmeCode.equals("RIT")){
-                spinner.setSelection(1);
-            }
 
-            editTextYear.setText(subscribeData.charAt(decodeCurrentCursor + 6)+"");
-            decodeCurrentCursor += 6;
-
-            char passwordLengthFirstDigit = subscribeData.charAt(decodeCurrentCursor+1);
-            char passwordLengthSecondDigit = subscribeData.charAt(decodeCurrentCursor+2);
-            decodeCurrentCursor += 2;
-            int passwordSize = Integer.parseInt(String.valueOf(passwordLengthFirstDigit)+String.valueOf(passwordLengthSecondDigit));
-            editTextPassword.setText(hexToAscii(splitString(passwordSize,subscribeData)));
         }else{
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Data is not completed")
@@ -82,7 +147,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnEncode(View v) {
-        String encodeText = "";
+        decodeCurrentCursor = 0;
+        String encodeText = "60"; // <-- Action code 6, reserve code 0.
         String name = editTextName.getText().toString();
         String password = editTextPassword.getText().toString();
         String gender = "";
@@ -94,11 +160,13 @@ public class MainActivity extends AppCompatActivity {
         String programmeCode = spinner.getSelectedItem().toString();
         String numOfYear = "00000" + editTextYear.getText().toString();
 
+        //Turn ASCll name, programme code and passsword to HEX.
         encodeText += name.length() + asciiToHex(name) + gender + asciiToHex(programmeCode) + numOfYear
                 + password.length() + asciiToHex(password);
 
         txtViewEncodeResult.setText("Encode Result: \n" + encodeText);
 
+        //Turn whole ASCll String to byte (If necessary)
         byte[] b = new BigInteger(encodeText,16).toByteArray();
 
         for(int i=0; i < b.length; i++){
@@ -108,15 +176,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //This function perform split string, parameter is size of String and String data
     public String splitString(int length, String subscribeData){
         String completeString = "";
-        for(int i = 0 ; i < length*2 ; i++){
-            decodeCurrentCursor ++;
-            completeString += subscribeData.charAt(decodeCurrentCursor);
+        for(int i = 0 ; i < length*2 ; i++){ // Length *2 because 1 byte have 2 bit
+            decodeCurrentCursor ++;          // Every loop cursor increase 1
+            completeString += subscribeData.charAt(decodeCurrentCursor); // Store every char split out into string
         }
         return completeString;
     }
 
+    //Turn Hex to ASCll For example : 31 turn to 1
     public String hexToAscii(String hexStr) {
         StringBuilder output = new StringBuilder("");
         for (int i = 0; i < hexStr.length(); i += 2) {
@@ -126,6 +196,7 @@ public class MainActivity extends AppCompatActivity {
         return output.toString();
     }
 
+    //Turn ASCll to Hex For example : 1 turn to 31
     public String asciiToHex(String asciiStr) {
         char[] chars = asciiStr.toCharArray();
         StringBuilder hex = new StringBuilder();
